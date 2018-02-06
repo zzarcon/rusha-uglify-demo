a3c857a02af24f821d909546a4e3ee1af3b670cd
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-02-06 11:03:27 +1100

    Add uglify script

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // import * as Rusha from 'rusha';
 import Rusha from 'rusha';
 
+console.log(Rusha)
+console.log(Rusha.createWorker)
+
 const estimatedChunkSize = 4 * 1024 * 1024; // hint for Rusha
 const arrayBuffer = [];
 
@@ -8,8 +11,12 @@ const r = new Rusha(estimatedChunkSize).digestFromArrayBuffer(
   arrayBuffer,
 );
 
+console.log(r)
+
 const worker = Rusha.createWorker();
 const hasherWorker = { worker, activeJobs: 0 };
+
+console.log(worker)
 
 worker.addEventListener('message', (event) => {
   this.handleWorkerMessage(event, hasherWorker);
